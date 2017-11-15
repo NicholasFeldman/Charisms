b04f45d2157ace83240e9ed4508c205309b01136
Nicholas Feldman <nickcfeldman@gmail.com>
2017-11-15 01:04:39 -0500

    Add Firebase

---
+++ src/components/QuotesPane.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p v-for="quote in quotes">
+      {{quote}}
+    </p>
+  </div>
+</template>
+
+<script>
+  import db from '../firebase'
+
+  const quotesRef = db.ref('/quotes')
+
+  export default {
+    name: 'quotes-pane',
+    firebase: {
+      quotes: quotesRef
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
