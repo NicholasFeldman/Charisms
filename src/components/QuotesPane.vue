95f7eeaa69269596499d1b0ce275e91fd62a24d1
Nicholas Feldman <nickcfeldman@gmail.com>
2017-11-15 02:10:57 -0500

    Render quotes in cards

--- src/components/QuotesPane.vue
+++ src/components/QuotesPane.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    <p v-for="quote in quotes">
-      {{quote}}
-    </p>
+    <md-card v-for="quote in quotes">
+      <md-card-header>
+        <div class="md-title">{{quote.sayer}}</div>
+        <div class="md-subhead">{{quote.title}}</div>
+      </md-card-header>
+
+      <md-card-content>
+        {{quote.quote}}
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button class="md-icon-button">
+          <md-icon>favorite</md-icon>
+        </md-button>
+      </md-card-actions>
+    </md-card>
   </div>
 </template>
 
@@ -18,3 +31,10 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .md-card {
+    margin: 4px;
+    display: inline-block;
+  }
+</style>
