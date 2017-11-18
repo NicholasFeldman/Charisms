b604d35e77d90bdb9fccf0836127305f916fc00c
Nicholas Feldman <nickcfeldman@gmail.com>
2017-11-17 21:22:17 -0500

    Split quote into separate component

--- src/components/QuotesPane.vue
+++ src/components/QuotesPane.vue
@@ -1,40 +1,25 @@
 <template>
   <div>
-    <md-card v-for="quote in quotes">
-      <md-card-header>
-        <div class="md-title">{{quote.sayer}}</div>
-        <div class="md-subhead">{{quote.title}}</div>
-      </md-card-header>
-
-      <md-card-content>
-        {{quote.quote}}
-      </md-card-content>
-
-      <md-card-actions>
-        <md-button class="md-icon-button">
-          <md-icon>favorite</md-icon>
-        </md-button>
-      </md-card-actions>
-    </md-card>
+    <single-quote
+      v-for="quote in quotes"
+      :key="quote['.key']"
+      :quote="quote"
+    />
   </div>
 </template>
 
 <script>
   import db from '../firebase'
 
+  import SingleQuote from './SingleQuote'
+
   const quotesRef = db.ref('/quotes')
 
   export default {
     name: 'quotes-pane',
+    components: { SingleQuote },
     firebase: {
       quotes: quotesRef
     }
   }
 </script>
-
-<style lang="scss" scoped>
-  .md-card {
-    margin: 4px;
-    display: inline-block;
-  }
-</style>
---
+++ src/components/SingleQuote.vue
@@ -0,0 +1,33 @@
+<template>
+  <md-card>
+    <md-card-header>
+      <div class="md-title">{{quote.sayer}}</div>
+      <div class="md-subhead">{{quote.title}}</div>
+    </md-card-header>
+
+    <md-card-content>
+      {{quote.quote}}
+    </md-card-content>
+
+    <md-card-actions>
+      <md-button class="md-icon-button" @click="likeQuote()">
+        <md-icon>favorite</md-icon>
+      </md-button>
+    </md-card-actions>
+  </md-card>
+</template>
+
+<script>
+  export default {
+    props: {
+      quote: Object
+    }
+  }
+</script>
+
+<style scoped>
+  .md-card {
+    margin: 4px;
+    display: inline-block;
+  }
+</style>
