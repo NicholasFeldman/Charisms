d0dd86074e139c52b7d7cea2732dbc18c8fd1d2d
Nicholas Feldman <nickcfeldman@gmail.com>
2018-02-06 23:30:47 -0500

    Update to new vue-cli & trim old dependencies
    Fixes #1
    Fixes #2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueFire from 'vuefire'
 
@@ -18,10 +16,7 @@ Vue.use(VueMaterial)
 
 Vue.use(Vuelidate)
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
-})
+  render: (h) => h(App),
+}).$mount('#app')
