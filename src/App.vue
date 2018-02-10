9975c8cb02fe8838bd071350a06214890fe947d8
Nicholas Feldman <nickcfeldman@gmail.com>
2018-02-10 14:30:11 -0500

    Preliminary app frame

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,58 @@
 <template>
-  <md-app>
+  <v-app>
 
-    <md-app-toolbar class="md-primary">
-      <div class="md-toolbar-section-start">
-        <span class="md-title">Charisms</span>
-      </div>
+    <v-toolbar app color="primary">
+      <v-toolbar-side-icon  @click.stop="drawer = !drawer"></v-toolbar-side-icon>
 
-      <div class="md-toolbar-section-end">
-        <md-button
-          v-if="!currentUser"
-          class="md-primary"
-          @click="signIn()"
-        >Log in</md-button>
+      <v-spacer></v-spacer>
 
-        <md-button
-          v-if="currentUser"
-          class="md-primary"
-          @click="signOut()"
-        >Log Out</md-button>
-      </div>
+      <v-btn
+        v-if="!currentUser"
+        @click="signIn()"
+      >Log in</v-btn>
 
-    </md-app-toolbar>
+      <v-btn
+        v-if="currentUser"
+        @click="signOut()"
+      >Log Out</v-btn>
+    </v-toolbar>
 
-    <md-app-drawer md-permanent="clipped">
-      <md-list>
-        <md-list-item to="/">
-          <md-icon>list</md-icon>
-          <span class="md-list-item-text">Quotes</span>
-        </md-list-item>
-      </md-list>
-    </md-app-drawer>
+    <v-navigation-drawer app fixed v-model="drawer">
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              Charisms
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-list>
+        <v-list-tile to="/">
+          <v-list-tile-action>
+            <v-icon>list</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Quotes</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
 
-    <md-app-content>
+    <v-content>
       <router-view/>
-    </md-app-content>
+    </v-content>
 
-  </md-app>
+  </v-app>
 </template>
 
 <script>
   import AuthMixin from './mixins/AuthMixin'
 
   export default {
-    mixins: [AuthMixin]
+    mixins: [AuthMixin],
+    data: () => ({
+      drawer: null,
+    })
   }
 </script>
